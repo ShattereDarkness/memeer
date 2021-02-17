2a5aebd196cf543e8dec047633944e1ad51725a7
man-of-panda3d <68993297+man-of-panda3d@users.noreply.github.com>
2021-02-17 10:54:26 +0530

    s

--- p3dfunc.py
+++ p3dfunc.py
@@ -14,22 +14,10 @@ def serialize (universe = {}, animation = []):
 	print ("animation:", animation)
 	for ano, anime in enumerate(animation):
 		print ("anime:", anime)
-		series = globals()[anime['funct']['fname']] (universe = universe, sindex = sindex, tag = anime['funct']['ftag'], specs = anime['specs'], model = anime['mmaps'])
+		series = globals()[anime['fname']] (universe = universe, sindex = sindex, tag = anime['ftag'], specs = anime['specs'], model = anime['mmaps'])
+		print (series)
 		sindex = mergedata (sindex, series)
 	return retval
-
-def setframelen (sindex = 0, specs = {}, conflen = 120):
-	fstart, flast = 0, 10
-	if len(specs['frames']) == 2 and specs['frames'][1] > 0:
-		fstart = specs['frames'][0]
-		flast = specs['frames'][1]
-	elif len(specs['frames']) == 2 and specs['frames'][1] == -1:
-		fstart = specs['frames'][0]
-		flast = specs['frames'][0] + conflen
-	else:
-		fstart = sindex
-		flast = sindex + conflen
-	return [fstart, flast]
 
 def mergeposition (base = [], addit = []):
 	retval = [0, 0, 0, 0, 0, 0, 1, 1, 1]
@@ -37,20 +25,51 @@ def mergeposition (base = [], addit = []):
 	for idx in range(6, 9): retval[idx] = base[idx] * addit[idx]
 	return retval
 
-def setmodelpost (modpos = [0, 0, 0, 0, 0, 0, 1, 1, 1], specs = {}, fcount = 10):
+def setframelen (sindex = 0, specs = {}, conflen = 120):
+	fstart, flast = sindex, sindex+conflen
+	if len(specs['frames']) == 2:
+		if specs['frames'][1] != -1:
+			fstart = specs['frames'][0]
+			flast = specs['frames'][1]
+		else:
+			fstart = specs['frames'][0]
+			flast = specs['frames'][0] + conflen
+	return [fstart, flast]
+
+def setscreentext (specs = {}, fcount=1):
 	retval = []
+	scount = len(specs['sttmts'])
+	if scount == 0: return retval
+	fcount = frames[1] - frames[0]
+	ltext = ''
+	if fcount < scount:
+		retval.append("\n".join(specs['sttmts']))
+		return retval
+	ratio = fcount//scount
+	for sx in range(0, scount):
+		frid = sx * ratio
+		if len(retval) < frid:
+			for fx in range (len(retval), frid): retval.append('')
+		retval.append(ltext + specs['sttmts'][sx])
+		ltext = ltext + specs['sttmts'][sx] + "\n"
+	return retval
+
+def setmodelpost (universe, gmodel, specs, fcount, wtfunc):
+	basepos = modfpos = modlpos = gmodel['xyz'] + gmodel['hpr'] + gmodel['lbh']
 	if len(specs['locpos']) != 0:
-		modpos = mergeposition (base = modpos, addit = specs['locpos'])
-	if len(specs['locfrom']) == 0 or len(specs['locupto']) == 0:
-		return [modpos]
-	locfrom = mergeposition (base = modpos, addit = specs['locfrom'])
-	retval.append(locfrom)
-	locupto = mergeposition (base = modpos, addit = specs['locupto'])
-	for frid in range (1, fcount):
+		modfpos = modlpos = mergeposition (base = basepos, addit = specs['locpos'])
+	if len(specs['locfrom']) == 0 and len(specs['locupto']) == 0:
+		return [modfpos]
+	modfpos = mergeposition (base = basepos, addit = specs['locfrom'])
+	modlpos = mergeposition (base = basepos, addit = specs['locupto'])
+	retval = [modfpos]
+	wtlist = range (1, fcount)
+	for frid in wtlist:
 		modpos = []
 		for pix in range(0, 9):
-			modpos.append(locfrom[pix] + ((locupto[pix] - locfrom[pix])*(frid)/(fcount-1)))
+			modpos.append(modfpos[pix] + ((modlpos[pix] - modfpos[pix])*(frid)/(fcount+1)))
 		retval.append(modpos)
+	retval.append(modlpos)
 	return retval
 
 def objectlay (universe = {}, sindex = 0, tag = 'text', specs = {}, model = {}):
@@ -58,12 +77,54 @@ def objectlay (universe = {}, sindex = 0, tag = 'text', specs = {}, model = {}):
 	retval = {'func': 'pass', 'file': '', 'post': [], 'sttmts': specs['sttmts']}
 	obj = {}
 	if tag == 'text': obj = model['2']
+	if tag == 'singl': obj = model['0']
 	if obj == {}: return retval
 	gmodel = universe['objects'][obj[0]['gmodel']]
 	smodel = obj[0]['smodel']
-	retval['file'] = gmodel['model'][smodel]['file']
-	retval['func'] = 'loadmodel'
-	retval['frames'] = frames = setframelen (sindex = sindex, specs = specs, conflen = defaults['length'])
-	modpos = gmodel['xyz'] + gmodel['hpr'] + gmodel['lbh']
-	retval['post'] = setmodelpost (modpos = modpos, specs = specs, fcount = frames[1]-frames[0])
+	frames = setframelen (sindex = sindex, specs = specs, conflen = defaults['length'])
+	sttmt = setscreentext (specs = specs, fcount = frames[1]-frames[0])
+	posts = setmodelpost (universe, gmodel, specs, frames[1]-frames[0], 'default')
+	return ({'model': gmodel['model'][smodel]['file'], 'frames': frames, 'posts': posts, 'sttmt': sttmt})
+
+def getactorfile (universe, gmodel, smodel, action):
+	retval = {}
+	basef = gmodel['model'][0]['file']
+	mfile = gmodel['model'][smodel]['file']
+	acts = {}
+	actf = {'speed': 1, 'fstart': 0, 'flast': 1}
+	for actf in gmodel['acts']:
+		actn, actdet = list(actf.items())[0][0], list(actf.items())[0][1]
+		acts[actn] = basef + "_" + actn
+		if actn != action: continue
+		actf = {'action': action,'speed': actdet['speed'], 'fstart': actdet['fstart'], 'flast': actdet['flast']}
+	return {'mfile': mfile, 'acts': acts, 'actf': actf}
+
+def setactorpose (actfnc, fcount):
+	retval = []
+	ix = 0
+	while (ix <= fcount+1):
+		fx = actfnc['fstart']
+		for fx in range(actfnc['fstart'], actfnc['flast']+1):
+			retval.append({'pose': fx})
+			ix = ix + 1
+			if ix == fcount: return retval
+		retval.append({'func': 'poschk'})
+		if ix == fcount: return retval
 	return retval
+
+def actordoes (universe = {}, sindex = 0, tag = 'text', specs = {}, model = {}):
+	retval = {'func': 'pass', 'file': '', 'post': [], 'sttmts': specs['sttmts']}
+	obj = {}
+	if tag == 'text':
+		obj = model['0']
+		act = model['1']
+	if obj == {}: return retval
+	gmodel = universe['objects'][obj[0]['gmodel']]
+	smodel = obj[0]['smodel']
+	action = universe['actions'][act[0]['gmodel']]['func']
+	actor = getactorfile (universe, gmodel, smodel, action)
+	frames = setframelen (sindex = sindex, specs = specs, conflen = actor['actf']['flast']-actor['actf']['fstart'])
+	sttmt = setscreentext (specs = specs, fcount = frames[1]-frames[0])
+	posts = setmodelpost (universe, gmodel, specs, frames[1]-frames[0], 'default')
+	poses = setactorpose (actfnc = actor['actf'], fcount = frames[1]-frames[0])
+	return ({'actor': actor, 'frames': frames, 'posts': posts, 'poses': poses})
